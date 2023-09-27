3b765a66d4deb1d36e798872a907ea63a645b14e
Jose Sepulveda <josealejandrosepulvedapino@gnail.com>
2023-09-26 22:04:32 -0300

    feat: issue 1 filter price and 24h change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-
+import { HttpClient } from '@angular/common/http';
 
 interface Criptomoneda {
   id: string;
@@ -8,21 +7,16 @@ interface Criptomoneda {
   symbol: string;
   image: string;
   current_price: number;
-  price_change_percentage_24h: number
+  price_change_percentage_24h: number;
   total_volume: number;
-
-
-  
 }
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   criptomonedas: Criptomoneda[] = [];
   titulos: string[] = [
     '#',
@@ -30,30 +24,89 @@ export class AppComponent implements OnInit {
     'Precio Actual (USD)',
     'Cambio 24hrs',
     'Volumen total',
-  ]
+  ];
   buscarTexto: string = '';
   criptomonedasFiltradas: Criptomoneda[] = [];
+  precioAscendente: boolean = false;
+  cambioAscendente: boolean = false;
 
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   buscarCriptomoneda() {
-    this.criptomonedasFiltradas = this.criptomonedas.filter(criptomoneda => 
-      criptomoneda.name.toLocaleLowerCase().includes(this.buscarTexto.toLocaleLowerCase()) ||
-      criptomoneda.symbol.toLocaleLowerCase().includes(this.buscarTexto.toLocaleLowerCase()))
+    this.criptomonedasFiltradas = this.criptomonedas.filter(
+      (criptomoneda) =>
+        criptomoneda.name
+          .toLocaleLowerCase()
+          .includes(this.buscarTexto.toLocaleLowerCase()) ||
+        criptomoneda.symbol
+          .toLocaleLowerCase()
+          .includes(this.buscarTexto.toLocaleLowerCase())
+    );
   }
 
-  ngOnInit(){
-    this.http.get<Criptomoneda[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .subscribe( (resp) => {;
-      this.criptomonedas = resp
-      this.criptomonedasFiltradas = resp;
-    }, 
-    (error) => console.log(error))
-  };
+  ngOnInit() {
+    this.http
+      .get<Criptomoneda[]>(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .subscribe(
+        (resp) => {
+          this.criptomonedas = resp;
+          this.criptomonedasFiltradas = resp;
+        },
+        (error) => console.log(error)
+      );
+  }
 
+  filtrarPorPrecio() {
+    if (this.precioAscendente) {
+      this.criptomonedasFiltradas = this.criptomonedasFiltradas.sort((a, b) => {
+        if (a.current_price < b.current_price) {
+          return 1;
+        }
+        if (a.current_price > b.current_price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    if (!this.precioAscendente) {
+      this.criptomonedasFiltradas = this.criptomonedasFiltradas.sort((a, b) => {
+        if (a.current_price > b.current_price) {
+          return 1;
+        }
+        if (a.current_price < b.current_price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    this.precioAscendente = !this.precioAscendente;
+  }
 
-
+  filtrarPorCambio() {
+    if (this.precioAscendente) {
+      this.criptomonedasFiltradas = this.criptomonedasFiltradas.sort((a, b) => {
+        if (a.price_change_percentage_24h < b.price_change_percentage_24h) {
+          return 1;
+        }
+        if (a.price_change_percentage_24h > b.price_change_percentage_24h) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    if (!this.precioAscendente) {
+      this.criptomonedasFiltradas = this.criptomonedasFiltradas.sort((a, b) => {
+        if (a.price_change_percentage_24h > b.price_change_percentage_24h) {
+          return 1;
+        }
+        if (a.price_change_percentage_24h < b.price_change_percentage_24h) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    this.precioAscendente = !this.precioAscendente;
+  }
 }
